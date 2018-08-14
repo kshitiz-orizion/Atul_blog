db785a83040ef0b08d4914b9a8996996f27b1a4b
KSHITIZ <kshitiz.orizion@gmail.com>
2018-08-14 13:09:22 +0530

    2nd commit

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ mongoose.connection.once('connected', function() {
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended:false}));
-
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.use(function(req,res,next){
 	res.header('Access-control-Allow-Origin','*');
 	res.header('Access-control-Allow-Methods','GET,PUT,POST,DELETE');
